baede520a83c986b520fdd5862a6b6bcd5c0d17c
Sunghun Park <dfstem9@gmail.com>
2025-12-15 16:39:51 +0900

    AppImage - ImageAssets 추가 및 앱 DesignSystem/AppImage 정의

---
+++ Odaeri/DesignSystem/AppImage.swift
@@ -0,0 +1,48 @@
+//
+//  AppImage.swift
+//  Odaeri
+//
+//  Created by 박성훈 on 12/15/25.
+//
+
+import UIKit
+
+enum AppImage {
+    // MARK: - Category
+    static let bakery = UIImage(named: "Bakery")
+    static let coffee = UIImage(named: "Coffee")
+    static let dessert = UIImage(named: "Dessert")
+    static let fastFood = UIImage(named: "FastFood")
+    static let more = UIImage(named: "More")
+    
+    // MARK: - Icon
+    static let check = UIImage(named: "Check")
+    static let chevron = UIImage(named: "chevron")
+    static let `default` = UIImage(named: "Default")
+    static let detail = UIImage(named: "Detail")
+    static let distance = UIImage(named: "Distancere")
+    static let likeEmpty = UIImage(named: "Like_Empty")
+    static let likeFill = UIImage(named: "Like_Fill")
+    static let list = UIImage(named: "List")
+    static let location = UIImage(named: "Location")
+    static let parking = UIImage(named: "Parking")
+    static let run = UIImage(named: "Run")
+    static let search = UIImage(named: "Search")
+    static let sesac = UIImage(named: "Sesac")
+    static let starEmpty = UIImage(named: "Star_Empty")
+    static let starFill = UIImage(named: "Star_Fill")
+    static let time = UIImage(named: "Time")
+    static let write = UIImage(named: "Write")
+    
+    // MARK: - TabBar
+    static let communityEmpty = UIImage(named: "Community_Empty")
+    static let communityFill = UIImage(named: "Community_Fill")
+    static let homeEmpty = UIImage(named: "Home_Empty")
+    static let homeFill = UIImage(named: "Home_Fill")
+    static let orderEmpty = UIImage(named: "Order_Empty")
+    static let orderFill = UIImage(named: "Order_Fill")
+    static let pickEmpty = UIImage(named: "Pick_Empty")
+    static let pickFill = UIImage(named: "Pick_Fill")
+    static let profileEmpty = UIImage(named: "Profile_Empty")
+    static let profileFill = UIImage(named: "Profile_Fill")
+}
