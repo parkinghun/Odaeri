5f9ab013260b95ffd34058980fe842220e1bf476
Sunghun Park <dfstem9@gmail.com>
2025-12-16 13:58:15 +0900

    Admin 타겟 추가

---
+++ Odaeri_Admin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Odaeri_Admin
+//
+//  Created by 박성훈 on 12/16/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
