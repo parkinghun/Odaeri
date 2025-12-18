849e9d5aceb75315b54de7fa3b412643807b6e03
Sunghun Park <dfstem9@gmail.com>
2025-12-18 10:36:28 +0900

    BaseAPI - appversion 및 endpoint 연결

--- Shared/Network/Base/BaseAPI.swift
+++ Shared/Network/Base/BaseAPI.swift
@@ -9,12 +9,21 @@ import Foundation
 import Moya
 
 protocol BaseAPI: TargetType {
-    // 각 API에서 구현해야 할 것들
+    var apiVersion: String { get }
+    var endpoint: String { get }
 }
 
 extension BaseAPI {
+    var apiVersion: String {
+        return APIEnvironment.current.version
+    }
+
     var baseURL: URL {
         return APIEnvironment.current.baseURL
+    }
+
+    var path: String {
+        return "/\(apiVersion)\(endpoint)"
     }
 
     var headers: [String: String]? {
@@ -22,8 +31,6 @@ extension BaseAPI {
         headers["Content-Type"] = "application/json"
         headers["Accept"] = "application/json"
 
-        // 필요시 각 API에서 오버라이드하여 토큰 추가 가능
-        // AuthPlugin으로 처리할 수도 있음
         return headers
     }
 
