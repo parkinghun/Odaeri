49df52e2909002a80b28fe7e2df060637539437b
Sunghun Park <dfstem9@gmail.com>
2025-12-28 22:24:06 +0900

    AuthAPI - refreshToken 추가

--- Shared/Network/APIs/AuthAPI.swift
+++ Shared/Network/APIs/AuthAPI.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+import Moya
+
+enum AuthAPI {
+    case refreshToken(token: String)
+}
+
+extension AuthAPI: BaseAPI {
+    var endpoint: String {
+        switch self {
+        case .refreshToken:
+            return "/auth/refresh"
+        }
+    }
+
+    var requiresAuthentication: Bool {
+        return true
+    }
+
+    var method: Moya.Method {
+        switch self {
+        case .refreshToken:
+            return .get
+        }
+    }
+
+    var task: Task {
+        switch self {
+        case .refreshToken:
+            return .requestPlain
+        }
+    }
+
+    var headers: [String: String]? {
+        var headers = [String: String]()
+        headers["Accept"] = "application/json"
+        headers["SeSACKey"] = APIEnvironment.current.apiKey
+        headers["Authorization"] = TokenManager.shared.accessToken
+
+        
+        switch self {
+        case .refreshToken(let token):
+            headers["RefreshToken"] = token
+        }
+
+        return headers
+    }
+}
