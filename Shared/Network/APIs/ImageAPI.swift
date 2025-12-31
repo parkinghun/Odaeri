ba2f2544f5d596a294a4895857906a639f5be6ce
Sunghun Park <dfstem9@gmail.com>
2025-12-31 14:32:01 +0900

    ImageAPI 수정 - fetchImage case에 etag 관련 연관값 추가

--- Shared/Network/APIs/ImageAPI.swift
+++ Shared/Network/APIs/ImageAPI.swift
@@ -9,13 +9,13 @@ import Foundation
 import Moya
 
 enum ImageAPI {
-    case fetchImage(path: String)
+    case fetchImage(path: String, etag: String? = nil)
 }
 
 extension ImageAPI: BaseAPI {
     var endpoint: String {
         switch self {
-        case .fetchImage(let path):
+        case .fetchImage(let path, _):
             return path
         }
     }
@@ -31,9 +31,21 @@ extension ImageAPI: BaseAPI {
         case .fetchImage: .requestPlain
         }
     }
-    
+
     var headerSet: HeaderSet {
         return .readImage
+    }
+
+    // ETag 지원을 위한 headers override
+    var headers: [String: String]? {
+        var headers = headerSet.toHeaders()
+
+        // 304 응답을 위한 If-None-Match 헤더 추가
+        if case .fetchImage(_, let etag) = self, let etag = etag {
+            headers["If-None-Match"] = etag
+        }
+
+        return headers
     }
 }
 
