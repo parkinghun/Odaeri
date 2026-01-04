fac6db5b23bfd5f33c523d089526ed5ca674a9a4
Sunghun Park <dfstem9@gmail.com>
2026-01-04 22:39:20 +0900

    ShopDetail - vc, vm 구현

---
+++ Odaeri_User/Presentation/Home/ViewModels/ShopDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ShopDetailViewModel.swift
+//  Odaeri_User
+//
+//  Created by 박성훈 on 1/2/26.
+//
+
+import Foundation
---
+++ Odaeri_User/Presentation/Home/Views/Detail/ShopDetailViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ShopDetailViewController.swift
+//  Odaeri_User
+//
+//  Created by 박성훈 on 1/2/26.
+//
+
+import Foundation
