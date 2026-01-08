9214b7de27d7c74ead6ab3ee307b5a48896a0866
Sunghun Park <dfstem9@gmail.com>
2026-01-08 17:39:47 +0900

    Video 관련 Repository, Impl 구현

---
+++ Odaeri_User/Data/Repositories/VideoRepositoryImpl.swift
@@ -0,0 +1,36 @@
+//
+//  VideoRepositoryImpl.swift
+//  Odaeri_User
+//
+//  Created by 박성훈 on 1/10/26.
+//
+
+import Foundation
+import Combine
+import Moya
+
+final class VideoRepositoryImpl: VideoRepository {
+    private let provider = ProviderFactory.makeVideoProvider()
+
+    func fetchVideoList(next: String?, limit: Int?) -> AnyPublisher<[VideoEntity], NetworkError> {
+        provider.requestPublisher(.fetchVideoList(next: next, limit: limit))
+            .map { (response: VideoListResponse) in
+                response.data.map { VideoEntity(from: $0) }
+            }
+            .eraseToAnyPublisher()
+    }
+
+    func getVideoStreamingURL(videoId: String) -> AnyPublisher<VideoStreamEntity, NetworkError> {
+        provider.requestPublisher(.getVideoStreamingURL(videoId: videoId))
+            .map { (response: VideoStreamResponse) in
+                VideoStreamEntity(from: response)
+            }
+            .eraseToAnyPublisher()
+    }
+
+    func toggleVideoLike(videoId: String, status: Bool) -> AnyPublisher<Void, NetworkError> {
+        provider.requestPublisher(.toggleVideoLike(videoId: videoId, status: status))
+            .map { (_: EmptyResponse) in () }
+            .eraseToAnyPublisher()
+    }
+}
---
+++ Odaeri_User/Domain/RepositoryProtocols/VideoRepository.swift
@@ -0,0 +1,15 @@
+//
+//  VideoRepository.swift
+//  Odaeri_User
+//
+//  Created by 박성훈 on 1/10/26.
+//
+
+import Foundation
+import Combine
+
+protocol VideoRepository {
+    func fetchVideoList(next: String?, limit: Int?) -> AnyPublisher<[VideoEntity], NetworkError>
+    func getVideoStreamingURL(videoId: String) -> AnyPublisher<VideoStreamEntity, NetworkError>
+    func toggleVideoLike(videoId: String, status: Bool) -> AnyPublisher<Void, NetworkError>
+}
