fc35ac85f781e175891ffa1031768ab99b1f061e
Sunghun Park <dfstem9@gmail.com>
2026-01-09 13:56:44 +0900

    Profile Coordinator 구현

--- Odaeri_User/Presentation/Profile/Coordinators/ProfileCoordinator.swift
+++ Odaeri_User/Presentation/Profile/Coordinators/ProfileCoordinator.swift
@@ -22,13 +22,55 @@ final class ProfileCoordinator: Coordinator {
     }
 
     func start() {
-        let profileViewModel = ProfileViewModel()
-        profileViewModel.coordinator = self
-        let profileViewController = ProfileViewController(viewModel: profileViewModel)
-        navigationController.setViewControllers([profileViewController], animated: false)
+        let targetUserId = UserManager.shared.currentUser?.userId ?? ""
+        let viewModel = UserProfileViewModel(targetUserId: targetUserId)
+        viewModel.coordinator = self
+        let viewController = UserProfileViewController(viewModel: viewModel)
+        navigationController.setViewControllers([viewController], animated: false)
     }
 
     func didFinishLogout() {
         delegate?.profileCoordinatorDidFinishLogout(self)
     }
+
+    func showEditProfile() {
+        showPlaceholderAlert(title: "프로필 수정", message: "프로필 수정 화면은 준비 중입니다.")
+    }
+
+    func showSettings() {
+        showPlaceholderAlert(title: "설정", message: "설정 화면은 준비 중입니다.")
+    }
+
+    func showReportOptions(targetUserId: String) {
+        showPlaceholderAlert(title: "신고/차단", message: "신고/차단 기능은 준비 중입니다.")
+    }
+
+    func showWritePost() {
+        showPlaceholderAlert(title: "글쓰기", message: "게시글 작성 화면은 준비 중입니다.")
+    }
+
+    func showEditPost(postId: String) {
+        showPlaceholderAlert(title: "게시글 수정", message: "게시글 수정 화면은 준비 중입니다.")
+    }
+
+    func showChatRoom(roomId: String, title: String?) {
+        let chatCoordinator = ChatCoordinator(navigationController: navigationController)
+        chatCoordinator.delegate = self
+        addChild(chatCoordinator)
+        chatCoordinator.showChatRoom(roomId: roomId, title: title)
+    }
+
+    private func showPlaceholderAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default))
+        navigationController.present(alert, animated: true)
+    }
 }
+
+extension ProfileCoordinator: ChatCoordinatorDelegate {
+    func chatCoordinatorDidFinish(_ coordinator: ChatCoordinator) {
+        removeChild(coordinator)
+    }
+}
+
+extension ProfileCoordinator: UserProfileCoordinating {}
