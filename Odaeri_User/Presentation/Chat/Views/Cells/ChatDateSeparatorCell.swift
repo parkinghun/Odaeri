2ebbb9f11cec76c9e056cff69581f4fbb8749c0e
Sunghun Park <dfstem9@gmail.com>
2026-01-09 14:46:53 +0900

    채팅 데이트 구분 셀 구현

---
+++ Odaeri_User/Presentation/Chat/Views/Cells/ChatDateSeparatorCell.swift
@@ -0,0 +1,45 @@
+//
+//  ChatDateSeparatorCell.swift
+//  Odaeri_User
+//
+//  Created by 박성훈 on 1/9/26.
+//
+
+import UIKit
+import SnapKit
+
+final class ChatDateSeparatorCell: UITableViewCell {
+    static let reuseIdentifier = String(describing: ChatDateSeparatorCell.self)
+
+    private let label: UILabel = {
+        let label = UILabel()
+        label.font = AppFont.caption1
+        label.textColor = AppColor.gray60
+        label.textAlignment = .center
+        return label
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupUI() {
+        selectionStyle = .none
+        contentView.backgroundColor = AppColor.gray0
+        contentView.addSubview(label)
+
+        label.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(AppSpacing.small)
+            $0.centerX.equalToSuperview()
+        }
+    }
+
+    func configure(text: String) {
+        label.text = text
+    }
+}
