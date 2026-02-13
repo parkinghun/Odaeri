33e3a69685f9b6ce2d17b8e86c1999b17b5b5392
Sunghun Park <dfstem9@gmail.com>
2026-02-13 18:46:32 +0900

    AppColor - 컬러셋 확정 및 적용

--- Shared/DesignSystem/AppColor.swift
+++ Shared/DesignSystem/AppColor.swift
@@ -8,31 +8,11 @@
 import UIKit
 
 enum AppColor {
-    // MARK: - Mint
     static let blackSprout = UIColor(hex: "#F28729")
     static let deepSprout = UIColor(hex: "#F7C578")
     static let brightSprout = UIColor(hex: "#F9DB5C")
     static let brightSprout2 = UIColor(hex: "#FFF8D9")
     static let brightForsythia = UIColor(hex: "#FDC020")
-    
-    // MARK: - Yellow
-//    static let blackSprout = UIColor(hex: "#f28729")
-//    static let deepSprout = UIColor(hex: "#f77029")
-//    static let brightSprout = UIColor(hex: "#F9DB5C")
-//    static let brightForsythia = UIColor(hex: "#bf0404")
-//    static let pointTextColor = UIColor(hex: "#392E1D")
-    
-    //MARK: - Pink
-//        static let blackSprout = UIColor(hex: "#E37D78")
-//        static let deepSprout = UIColor(hex: "#EBB5B3")
-//        static let brightSprout = UIColor(hex: "#FFE4E4")
-//        static let brightForsythia = UIColor(hex: "#FFCD4E")
-    
-    // MARK: - Original
-    //    static let blackSprout = UIColor(hex: "#82957B")
-    //    static let deepSprout = UIColor(hex: "#B7C8B1")
-    //    static let brightSprout = UIColor(hex: "#E0E4D9")
-    //    static let brightForsythia = UIColor(hex: "#FDC020")
     
     static let translucent90 = UIColor(hex: "#2D303199")
     static let errorRed = UIColor(hex: "#E64646")
